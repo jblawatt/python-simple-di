813c71c7306a923393eea4dd333c6643325e6b77
jblawatt <jblawatt@googlemail.com>
2014-04-21 14:26:26 +0200

    some doc changes

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,6 +15,8 @@
 import sys
 import os
 
+sys.path.append('../../')
+
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
@@ -104,7 +106,8 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'default'
+# html_theme = 'default'
+html_theme = 'haiku'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
